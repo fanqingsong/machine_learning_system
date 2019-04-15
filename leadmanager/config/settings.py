95fd2ca06373f6355caf0c7ad602602bb8608570
Qingsong Fan <qsfan@qq.com>
2019-04-16 00:52:35 +0800

    config name optimization

--- leadmanager/config/settings.py
+++ leadmanager/config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'leadmanager.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'leadmanager.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
