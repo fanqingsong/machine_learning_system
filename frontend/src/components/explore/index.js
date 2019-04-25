d14df5415b2ad3fe18494ac074714a8e48dc4b01
Qingsong Fan <qsfan@qq.com>
2019-04-26 01:11:10 +0800

    add single variable exploring

--- frontend/src/components/explore/index.js
+++ frontend/src/components/explore/index.js
@@ -6,6 +6,7 @@ import { getIris } from "../../actions/iris";
 import C3Chart from 'react-c3js';
 import 'c3/c3.css';
 
+import { Histogram, WrappedComponent, withParentSize, PatternLines, DensitySeries, BarSeries, XAxis, YAxis } from '@data-ui/histogram';
 
 export class IrisExplore extends Component {
   static propTypes = {
@@ -118,9 +119,136 @@ export class IrisExplore extends Component {
 
     let petalData = this.getPetalScatterData();
     let petalAxis = this.getPetalScatterAxis();
+    
+    const ResponsiveHistogram = withParentSize(({ parentWidth, parentHeight, ...rest}) => (
+      <Histogram
+        width={parentWidth}
+        height={parentHeight}
+        {...rest}
+      />
+    ));
+
+    const rawData11 = Array(100).fill().map(Math.random);
 
     return (
       <Fragment>
+        <h2>Iris Sepal Length</h2>
+        <div style={{height:"500px"}}>
+        <ResponsiveHistogram
+            orientation="vertical"
+            cumulative={false}
+            normalized={false}
+            binCount={20}
+            valueAccessor={datum => datum}
+            binType="numeric"
+            renderTooltip={({ event, datum, data, color }) => (
+              <div>
+                <strong style={{ color }}>{datum.bin0} to {datum.bin1}</strong>
+                <div><strong>count </strong>{datum.count}</div>
+                <div><strong>cumulative </strong>{datum.cumulative}</div>
+                <div><strong>density </strong>{datum.density}</div>
+              </div>
+            )}
+          >
+            <BarSeries
+              rawData={this.irisSeries.sepalLenSeries /* or binnedData={...} */}
+            />
+            <DensitySeries
+              rawData={this.irisSeries.sepalLenSeries /* or binnedData={...} */}
+            />
+            <XAxis />
+            <YAxis />
+          </ResponsiveHistogram>
+        </div>
+
+        <h2>Iris Sepal Width</h2>
+        <div style={{height:"500px"}}>
+        <ResponsiveHistogram
+            orientation="vertical"
+            cumulative={false}
+            normalized={false}
+            binCount={20}
+            valueAccessor={datum => datum}
+            binType="numeric"
+            renderTooltip={({ event, datum, data, color }) => (
+              <div>
+                <strong style={{ color }}>{datum.bin0} to {datum.bin1}</strong>
+                <div><strong>count </strong>{datum.count}</div>
+                <div><strong>cumulative </strong>{datum.cumulative}</div>
+                <div><strong>density </strong>{datum.density}</div>
+              </div>
+            )}
+          >
+            <BarSeries
+              rawData={this.irisSeries.sepalWidthSeries /* or binnedData={...} */}
+            />
+            <DensitySeries
+              rawData={this.irisSeries.sepalWidthSeries /* or binnedData={...} */}
+            />
+            <XAxis />
+            <YAxis />
+          </ResponsiveHistogram>
+        </div>
+
+        <h2>Iris Petal Length</h2>
+        <div style={{height:"500px"}}>
+        <ResponsiveHistogram
+            orientation="vertical"
+            cumulative={false}
+            normalized={false}
+            binCount={20}
+            valueAccessor={datum => datum}
+            binType="numeric"
+            renderTooltip={({ event, datum, data, color }) => (
+              <div>
+                <strong style={{ color }}>{datum.bin0} to {datum.bin1}</strong>
+                <div><strong>count </strong>{datum.count}</div>
+                <div><strong>cumulative </strong>{datum.cumulative}</div>
+                <div><strong>density </strong>{datum.density}</div>
+              </div>
+            )}
+          >
+            <BarSeries
+              rawData={this.irisSeries.petalLenSeries /* or binnedData={...} */}
+            />
+            <DensitySeries
+              rawData={this.irisSeries.petalLenSeries /* or binnedData={...} */}
+            />
+            <XAxis />
+            <YAxis />
+          </ResponsiveHistogram>
+        </div>
+
+
+        <h2>Iris Petal Width</h2>
+        <div style={{height:"500px"}}>
+        <ResponsiveHistogram
+            orientation="vertical"
+            cumulative={false}
+            normalized={false}
+            binCount={20}
+            valueAccessor={datum => datum}
+            binType="numeric"
+            renderTooltip={({ event, datum, data, color }) => (
+              <div>
+                <strong style={{ color }}>{datum.bin0} to {datum.bin1}</strong>
+                <div><strong>count </strong>{datum.count}</div>
+                <div><strong>cumulative </strong>{datum.cumulative}</div>
+                <div><strong>density </strong>{datum.density}</div>
+              </div>
+            )}
+          >
+            <BarSeries
+              rawData={this.irisSeries.petalWidthSeries /* or binnedData={...} */}
+            />
+            <DensitySeries
+              rawData={this.irisSeries.petalWidthSeries /* or binnedData={...} */}
+            />
+            <XAxis />
+            <YAxis />
+          </ResponsiveHistogram>
+        </div>
+
         <h2>Iris Sepal Scatter</h2>
         <C3Chart data={sepalData} axis={sepalAxis} />
 
